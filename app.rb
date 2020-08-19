cce71c697141e86fbe305c55dd5746642e3d0987
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-19 15:01:17 +0100

    entering names refactored

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
-    "Testing infrastructure working!"
+    erb(:index)
+  end
+
+  post '/names' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect "/play"
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb(:play)
   end
 
   #starts the server if ruby file executed directly
