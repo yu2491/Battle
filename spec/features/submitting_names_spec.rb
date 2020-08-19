3a7a0051cdb6b55f202078b041386895183d0a90
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-19 15:59:37 +0100

    helper

--- spec/features/submitting_names_spec.rb
+++ spec/features/submitting_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'players can enter their names' do
   scenario 'names can be filled in and then seen on screen' do
-    visit('/')
-    fill_in :player1, with: 'Yaz'
-    fill_in :player2, with: 'Charlotte'
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Yaz vs Charlotte!'
   end
 end
